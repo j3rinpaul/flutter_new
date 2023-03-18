0459fe16c27aaef1b29595ffa5c94dd2e09706aa
j3rin <jerrymic2255@gmail.com>
2023-03-18 16:37:32 +0530

    video 8 complete

--- lib/screen_home.dart
+++ lib/screen_home.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Center(
         child: Container(
-          child: Text(_counter.toString()),
+          child: DispalyText(textValue: _counter.toString()),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -38,5 +38,18 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Icon(Icons.add),
       ),
     );
+  }
+}
+
+//when we changed the text into a stateless widget as the whole page is being reloaded
+//the constructer is called each time and the value inside the stateless page is being changed
+class DispalyText extends StatelessWidget {
+  final String textValue;
+//this widget is being created again and again as the stateful screen changes
+  const DispalyText({super.key, required this.textValue});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(textValue);
   }
 }
